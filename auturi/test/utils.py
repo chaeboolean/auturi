040632a190c75b8eab287b000267b35d08cfdbd5
chaehyun <chjeong9727@snu.ac.kr>
2023-01-02 00:45:03 +0900

    test utils

--- auturi/test/utils.py
+++ auturi/test/utils.py
@@ -8,8 +8,8 @@ import gym
 import numpy as np
 import ray
 import torch
-
 from auturi.executor.environment import AuturiEnv
+from auturi.executor.gym_utils import get_action_sample
 from auturi.executor.policy import AuturiPolicy
 
 
@@ -44,6 +44,24 @@ class Timeout:
         self.handle_timeout(None, None)
 
 
+def make_space(is_action_discrete: bool):
+    if is_action_discrete:
+        action_space = gym.spaces.Discrete(10)
+    else:
+        action_space = gym.spaces.Box(low=-10, high=30, shape=(3,), dtype=np.float32)
+    obs_space = gym.spaces.Box(low=-10, high=30, shape=(5, 2), dtype=np.float32)
+
+    return obs_space, action_space
+
+
+def make_dummy_action(is_action_discrete: bool, num: int):
+    action_sample = make_space(is_action_discrete)[1].sample()
+    if is_action_discrete:
+        return np.stack([np.array([action_sample])] * num)
+    else:
+        return np.stack([action_sample] * num)
+
+
 class DumbEnv(AuturiEnv):
     """Dumb environment class for testing.
 
@@ -53,30 +71,28 @@ class DumbEnv(AuturiEnv):
 
     """
 
-    def __init__(self, idx):
+    def __init__(self, idx, is_action_discrete):
         self.idx = idx
         self.init_value = 1000 * (idx + 1)
         self.sleep = 0.5
 
-        self.action_space = gym.spaces.Box(
-            low=-10, high=30, shape=(5, 2), dtype=np.float32
-        )
-        self.observation_space = gym.spaces.Box(
-            low=-10, high=30, shape=(5, 2), dtype=np.float32
-        )
-        self.artifacts_samples = [self.action_space.sample()]
+        self.observation_space, self.action_space = make_space(is_action_discrete)
         self.metadata = None
 
-        self.value = self.observation_space.sample()
+        self.value = np.expand_dims(self.observation_space.sample(), 0)
+        self.artifacts_samples = [get_action_sample(self.action_space, 1)]
+
         self.storage = defaultdict(list)
+
+        self._validate()
 
     def step(self, action, _):
         time.sleep(self.sleep)
-        self.value += action
+        self.value += action.flatten()[0]
         self.storage["obs"].append(np.copy(self.value))
         self.storage["action"].append(np.copy(action))
 
-        return np.copy(self.value)  # , -2.3, False, {}
+        return np.copy(self.value)
 
     def reset(self):
         self.storage.clear()
@@ -98,6 +114,13 @@ def check_timeout(elapsed, timeout):
     assert timeout <= elapsed and timeout + 0.5 >= elapsed
 
 
+def check_array(given, answer, keep_order=True):
+    if keep_order:
+        assert np.all(given == np.array(answer))
+    else:
+        assert set(given) == set(answer)
+
+
 class DumbPolicy(AuturiPolicy):
     """Dumb policy class for testing.
 
@@ -108,42 +131,62 @@ class DumbPolicy(AuturiPolicy):
 
     """
 
-    def __init__(self, idx):
+    def __init__(self, idx, is_action_discrete):
         self.idx = idx
         self.init_value = 0 if idx == 0 else math.pow(10, idx)
         self.sleep = 1
         self.device = None
-        self.observation_space = gym.spaces.Box(
-            low=-10, high=30, shape=(5, 2), dtype=np.float32
-        )
-        self.value = self.observation_space.sample()
+        self.observation_space, self.action_space = make_space(is_action_discrete)
+
+        self.action_sample = get_action_sample(self.action_space, 1)
+        self._validate()
 
     def load_model(self, model, device):
         self.device = device
-        self.value.fill(self.init_value)
+        self.action_sample.fill(self.init_value)
 
+    # TODO: consider obs dimension.
     def compute_actions(self, obs, n_steps):
         time.sleep(self.sleep)
-        self.value += 1
-        return np.copy(self.value), [np.copy(self.value)]
+        self.action_sample += 1
+        actions = np.concatenate([self.action_sample] * obs.shape[0])
+
+        return actions, [actions]
 
     def terminate(self):
         pass
 
 
-def _create_fn(idx):
-    return DumbEnv(idx)
+class DummyPolicyHandler:
+    def __init__(self, out: np.ndarray):
+        self.out = out
+
+    def start_loop(self):
+        pass
+
+    def stop_loop(self):
+        pass
+
+    def compute_actions(self, *args, **kwargs):
+        return self.out
+
+    def terminate(self):
+        pass
 
 
-def create_env_fns(num_envs):
-    return [partial(_create_fn, idx) for idx in range(num_envs)]
+def _create_fn(idx, is_dicrete_action):
+    return DumbEnv(idx, is_dicrete_action)
 
 
-def create_policy_args():
+def create_env_fns(num_envs, is_dicrete_action):
+    return [partial(_create_fn, idx, is_dicrete_action) for idx in range(num_envs)]
+
+
+def create_policy_args(is_dicrete_action):
     model = torch.nn.Linear(10, 10, bias=False)
     model.eval()
     torch.nn.init.uniform_(model.weight, 1, 1)
-    return model, DumbPolicy, dict()
+    return model, DumbPolicy, {"is_action_discrete": is_dicrete_action}
 
 
 @ray.remote
